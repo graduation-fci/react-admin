faf271bc0e99166a7e15d698ae79eb431ea48714
Kareem Adel <101924125+imKareemAdel@users.noreply.github.com>
2023-05-21 16:10:04 +0300

    ..

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React  from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { BrowserRouter } from 'react-router-dom';
 import i18n from "i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
