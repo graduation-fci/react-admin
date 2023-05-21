97e2e3eb6cf707e5d8e0b1feb669c8b391b40318
Kareem Adel <101924125+imKareemAdel@users.noreply.github.com>
2023-05-21 17:53:50 +0300

    ..

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React  from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import { BrowserRouter } from 'react-router-dom';
 import i18n from "i18next";
